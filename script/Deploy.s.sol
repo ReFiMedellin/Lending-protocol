ea6c34a76504361f068bdc352d73cd7e7518e653
AnotherDev <joseluismanco37@gmail.com>
2024-04-09 11:36:07 -0500

    chore: correct deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
 import {LendResolver} from "../src/Resolver.sol";
-import {LendManager} from "../src/LendManager.sol";
+import {ReFiMedLend} from "../src/ReFiMedLend.sol";
 import {IEAS} from "@ethereum-attestation-service/contracts/IEAS.sol";
 
 contract Deploy is Script {
@@ -13,11 +13,9 @@ contract Deploy is Script {
         LendResolver lendResolver = new LendResolver(IEAS(0xC2679fBD37d54388Ce493F1DB75320D236e1815e));
         console.log("LendResolver address: ", address(lendResolver));
 
-        // Desplegar ContratoB con la dirección de ContratoA
-        LendManager lendManager = new LendManager(address(lendResolver));
+        ReFiMedLend lendManager = new ReFiMedLend(address(lendResolver));
         console.log("LendManager address: ", address(lendManager));
 
-        // Ejecutar una función de ContratoA a través de ContratoB
         lendResolver.setLendManager(address(lendManager));
 
         vm.stopBroadcast();
